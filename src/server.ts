3d1c0c47b62a2aa20b0ced242a716e0a37391bf0
Bruno Silva <bruno_silva2101@hotmail.com>
2023-09-12 21:16:45 -0300

    feat: first route and config project

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { fastify } from 'fastify'
+import { prisma } from './lib/prisma'
+import { getAllPromptsRoutes } from './routes/get-all-prompts'
+
+const app = fastify()
+
+app.register(getAllPromptsRoutes)
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server Running!')
+})
